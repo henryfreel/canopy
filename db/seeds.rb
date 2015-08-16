a4b7c80350a172e9e8e99edb280746abab61a1ab
Alex Chan <alex415@gmail.com>
2015-08-16 16:35:19 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: 'Alex', email: 'alex@gmail.com', password: 'password', location: 'San Francisco', bio: 'Put a bird on it Helvetica Pitchfork tofu dreamcatcher, selvage Intelligentsia mumblecore shabby chic gastropub quinoa. Squid Odd Future Wes Anderson fashion axe Tumblr, pour-over pop-up YOLO next level. Pickled narwhal scenester mumblecore blog distillery. ', github: 'www.github.com/alex415', linkedin: 'www.linkedin.com/alexchan415', stack: 'Javascript, Ruby on Rails, Express', job_title: 'Web Developer')
+
+User.create(username: 'Test', email: 'test@gmail.com', password: 'test', location: 'San Francisco', bio: 'Put a bird on it Helvetica Pitchfork tofu dreamcatcher, selvage Intelligentsia mumblecore shabby chic gastropub quinoa. Squid Odd Future Wes Anderson fashion axe Tumblr, pour-over pop-up YOLO next level. Pickled narwhal scenester mumblecore blog distillery. ', github: 'www.github.com/test', linkedin: 'www.linkedin.com/test', stack: 'Javascript, Ruby on Rails, Express', job_title: 'Web Developer')
+
+User.create(username: 'Zoe', email: 'zoe@gmail.com', password: 'password', location: 'San Francisco', bio: 'Put a bird on it Helvetica Pitchfork tofu dreamcatcher, selvage Intelligentsia mumblecore shabby chic gastropub quinoa. Squid Odd Future Wes Anderson fashion axe Tumblr, pour-over pop-up YOLO next level. Pickled narwhal scenester mumblecore blog distillery. ', github: 'www.github.com/zoe', linkedin: 'www.linkedin.com/zoe', stack: 'Javascript, Ruby on Rails, Express', job_title: 'Web Developer')
+
+6.times do
+  Project.create(
+  live_url: 'www.liveurl.herokuapp.com',
+  github_url: 'www.github.com/testproject',
+  title: FFaker::Lorem.words(3).join(' '),
+  description: FFaker::Lorem.words(20).join(' '),
+  user_id: 1,
+)
+end
+
+6.times do
+  Project.create(
+  live_url: 'www.liveurl.herokuapp.com',
+  github_url: 'www.github.com/testproject',
+  title: FFaker::Lorem.words(3).join(' '),
+  description: FFaker::Lorem.words(20).join(' '),
+  user_id: 2,
+)
+end
+
+6.times do
+  Project.create(
+  live_url: 'www.liveurl.herokuapp.com',
+  github_url: 'www.github.com/testproject',
+  title: FFaker::Lorem.words(3).join(' '),
+  description: FFaker::Lorem.words(20).join(' '),
+  user_id: 3,
+)
+end
