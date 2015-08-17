367922bc315a2efa24e8f73226d5c1d05fbe84b2
astp <anniepeng@Annies-MacBook-Pro.local>
2015-08-17 11:40:52 -0700

    added paperclip locally

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :projects, dependent: :destroy
   has_secure_password
 
+  has_attached_file :avatar,
+  					:styles => { :medium => "150x150>", :thumb => "44x44#" },
+  					:default_url => "/images/:style/missing.png"
+  validates_attachment :avatar, :presence => true,
+  						:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png", "image/jpg"]},
+  						:size => { :in => 0..10.kilobytes}
+
 end
