eeac17c9aeb197d5fb6ccaebc471c412a66885e8
Alex Chan <alex415@gmail.com>
2015-08-16 17:15:34 -0700

    added first and last name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815214736) do
+ActiveRecord::Schema.define(version: 20150817000622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20150815214736) do
     t.string   "stack"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
