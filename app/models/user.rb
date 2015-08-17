3e7fd56be8a84cac1d4231873b602d229305e308
astp <anniepeng@Annies-MacBook-Pro.local>
2015-08-17 14:00:25 -0700

    bucket works

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
   					:styles => { :medium => "150x150>", :thumb => "44x44#" },
-  					:default_url => "/images/:style/missing.png"
-  validates_attachment :avatar, :presence => true,
+  					:storage => :s3,
+  					:s3_credentials => Proc.new { |a| a.instance.s3_credentials },
+  					:path => "avatar/:id/:style/avatar.:extension",
+  					:default_url => "https://s3.amazonaws.com/project-canopy/defaults/default_avatar.png"
+  validates_attachment :avatar, 
   						:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png", "image/jpg"]},
-  						:size => { :in => 0..10.kilobytes}
+  						:size => { :in => 0..100.kilobytes}
+  def s3_credentials
+  	{:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['S3_PUBLIC_KEY'], :secret_access_key => ENV['S3_SECRET']}
+  end	
 
 end
