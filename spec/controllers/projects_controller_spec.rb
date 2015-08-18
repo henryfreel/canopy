97e7e16ef78dc5059d4717298cb326b550c751c2
zoejf <zoejfoss@gmail.com>
2015-08-18 12:26:32 -0700

    project controller #create test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  before do
+    user_params = Hash.new
+    user_params[:first_name] = FFaker::Name.first_name
+    user_params[:last_name] = FFaker::Name.last_name
+    user_params[:email] = FFaker::Internet.email
+    user_params[:password] = FFaker::Lorem.words(2).join
+    user_params[:password_confirmation] = user_params[:password]
+    @current_user = User.create(user_params)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+  end
 
-  describe "#new" 
+  describe "#new" do
     before do
-      user_params = Hash.new
-      user_params[:first_name] = FFaker::Name.first_name
-      user_params[:last_name] = FFaker::Name.last_name
-      user_params[:email] = FFaker::Internet.email
-      user_params[:password] = FFaker::Lorem.words(2).join
-      user_params[:password_confirmation] = user_params[:password]
-      @current_user = User.create(user_params)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
       get :new
     end
 
@@ -27,3 +29,27 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe "#create" do
+    before do
+      post :create, project: {title: "Hello", live_url: "www.test.com", github_url: "www.test.com", description: "lakfaiernefjsndfe adkjand"}
+    end
+
+    it "should respond with 302 found" do
+      expect(response.status).to be(302)
+    end
+
+    it "should redirect to project_path" do
+      expect(response.location).to match(/\/projects\/\d+/)
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
