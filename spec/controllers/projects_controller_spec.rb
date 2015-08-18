8d6c0bf76e51018356ab18e7046305997502d9c2
zoejf <zoejfoss@gmail.com>
2015-08-18 11:45:48 -0700

    projects controller and 1 user model tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 
-end
+  describe "#new" 
+    before do
+      user_params = Hash.new
+      user_params[:first_name] = FFaker::Name.first_name
+      user_params[:last_name] = FFaker::Name.last_name
+      user_params[:email] = FFaker::Internet.email
+      user_params[:password] = FFaker::Lorem.words(2).join
+      user_params[:password_confirmation] = user_params[:password]
+      @current_user = User.create(user_params)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+      get :new
+    end
+
+    it "should respond with 200 success" do
+      expect(response.status).to be(200)
+    end
+
+    it "should assign @project" do
+      expect(assigns(:project)).to be_instance_of(Project)
+    end 
+
+    it "should render the :new view" do
+      expect(response).to render_template(:new)
+    end
+  end
