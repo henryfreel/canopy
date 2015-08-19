819a5e3162fbba924f5623eb44f3062a21b39584
Henry Freel <henryfreel@gmail.com>
2015-08-18 20:54:19 -0700

    adds like functionality to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817220543) do
+ActiveRecord::Schema.define(version: 20150818210457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "live_url"
